a538d36ecdd91196b3cdc2b478c98bb189887884
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2022-01-01 16:19:30 +0000

    Make devServer configuration optional

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,11 +2,14 @@ var path = require('path')
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
-environment.config.devServer.watchContentBase = false
-environment.config.devServer.contentBase = [
-  path.join(__dirname, '../../app/views'),
-  path.join(__dirname, '../../app/helpers'),
-  path.join(__dirname, '../../app/reflexes')
-]
+
+if(environment.config.devServer) {
+  environment.config.devServer.watchContentBase = false
+  environment.config.devServer.contentBase = [
+    path.join(__dirname, '../../app/views'),
+    path.join(__dirname, '../../app/helpers'),
+    path.join(__dirname, '../../app/reflexes')
+  ]
+}
 
 module.exports = environment.toWebpackConfig()
